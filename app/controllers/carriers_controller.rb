72721b460cee4285d8afa00f0b865521dd677963
TOSHIOKANEDA <bhoosama@hotmail.com>
2022-05-25 01:31:16 +0900

    slight refit 0524

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -1,39 +1,52 @@
 class CarriersController < ApplicationController
 
-
   def index
   end
 
-  def new
-    @carrier = Carrier.new
+  def search
+    records = Carrier.find_carrier(date_params)
+    # Tariffを配列に持たせる
+    # 例）["SITC", "dem", "20FT DRY", ALL, 3000, 6000, 9000, 12000]
+    tariff = Utils::Tariff.new(records).tariff_table
+    # 日数のレンジを４段階で抽出する（全て１〜４まである前提）＜＝ない場合の挙動確認
+    # 例）["SITC", "dem", "20FT DRY", ALL, [1, 7], [8, 15], [15, 20], [21, 999999]]
+    range = Utils::Range.new(records).date_range
+    # レンジごとに日数のDate型で配列に格納し、二重配列にする
+    # 例）["SITC", "dem", "20FT DRY", ALL, [first date], [second date], [third date], [fourth date] ]
+    each_day = Utils::EachDay.new(date_params, range).period
+    # レンジごとにtariffとeach_dayの日数を掛けて金額をそれぞれ数値型で格納。レンジがなかった場合には0が入る。
+    # 例）["SITC", "dem", "20FT DRY", "ALL", 9000, 24000, 63000, 60000]
+    amount = Utils::Amount.new(tariff, each_day).calc
+ajax_hash(tariff)
+    respond_to do |format| # リクエスト形式によって処理を切り分ける
+      format.html { redirect_to :root }
+      # format.json { render json: break_down(tariff, date_params, each_day, range, amount) }
+    end
   end
 
-  def search
+  private
 
-    # xxx日
-    # 1. 日数分割をする[配列でとる]
-    # 2. Tariffを割り当てる
-    # 3. 合計を算出する
- 
-    records = Carrier.find_carrier(date_params)
+  def convert(data)
+    data == [] ? "" : data
+  end
 
-      # ["SITC", "dem", "20FT DRY", ALL, 3000, 6000, 9000, 12000]
-      tariff = Utils::Tariff.new(records).tariff_table
-p tariff
-      # ["SITC", "dem", "20FT DRY", ALL, [1, 7], [8, 15], [15, 20], [21, 999999]]
-      range = Utils::Range.new(records).date_range
-p range
-      # ["SITC", "dem", "20FT DRY", ALL, [first date], [second date], [third date], [fourth date] ]
-      each_day = Utils::EachDay.new(date_params, range).period
-p each_day
-      # ["SITC", "dem", "20FT DRY", "ALL", 9000, 24000, 63000, 60000]
-      amount = Utils::Amount.new(tariff, each_day).calc
-p amount
+  def port_convert(port)
+    # WHL, TSLの条件分岐をする
+    port.upcase == "ALL" ? "日本全港" : ""
+  end
 
-# p Carrier.container_types.keys[tariff[2].to_i].to_s
+  def free_period(start, each_day)
+    # each_day[4]がfirst_each_dayで、最初の日付の１日前
+    each_day.blank? ? ["",""] : [start, (each_day[0] - 1)]
+  end
 
-  break_down = {
-    carrier: tariff[0],
+  def date_params
+    params.permit(:port, :dem_det, :start, :finish, :calc, :type, :free, :carrier)
+  end
+
+  def break_down(tariff, date_params, each_day, range, amount) {
+    # ajaxに送り返すdataを、ハッシュで格納
+    carrier: tariff[0].upcase,
     dem_det: tariff[1].upcase,
     type: tariff[2],
     port: port_convert(tariff[3]),
@@ -59,31 +72,6 @@ p amount
     second_amount: amount[5],
     third_amount: amount[6],
     fourth_amount: amount[7],
-    total_amount: amount[4..7].sum
-  }
-    respond_to do |format| # リクエスト形式によって処理を切り分ける
-      format.html { redirect_to :root }
-      format.json { render json: break_down }
-    end
-  end
-
-  private
-
-  def convert(data)
-    data == [] ? "" : data
-  end
-
-  def port_convert(port)
-    # WHL, TSLの条件分岐をする
-    port.upcase == "ALL" ? "日本全港" : ""
-  end
-
-  def free_period(start, each_day)
-    # each_day[4]がfirst_each_dayで、最初の日付の１日前
-    each_day.blank? ? ["",""] : [start, (each_day[0] - 1)]
-  end
-
-  def date_params
-    params.permit(:port, :dem_det, :start, :finish, :calc, :type, :free, :carrier)
+    total_amount: amount[4..7].sum }
   end
 end
